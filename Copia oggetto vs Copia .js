a8c46d00d48c6a4dd2f1987489861afd52cfef35
giacomo13 <giacomomaggio13@gmail.com>
2022-03-04 17:09:06 +0100

    Prove

---
+++ Copia oggetto vs Copia .js
@@ -0,0 +1,12 @@
+let book = {
+    author:'Giacomo',
+    title: 'Volo',
+    pages: 13,
+  };
+  
+  let book2 = Object.assign({},book); //creo una copia dell'oggetto
+  let book3 = book; //copio il riferimento: se modifico book3, modifico anche book!
+
+  let v = [1,2,3]; 
+  let v2 = Array.from(v); //creo una copia dell'array
+  let v3 = v; //copio il riferimento all'array: se modifico v3, modifico anche v!
