27a9852ba5138a210e1846d115d255cf124ece59
giacomo13 <giacomomaggio13@gmail.com>
2022-04-05 11:20:40 +0200

    prova

---
+++ react/prova_react/src/Button.js
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import {Button} from "react-bootstrap";
+
+function Button(props){
+    
+    let [buttonLang,setButtonLang] = useState(props.lang);
+
+    if(buttonLang === 'it')
+    return <button onClick={()=>setButtonLang('en')}>Ciao!</button>;
+    else
+    return <button onClick={()=>setButtonLang('it')}>Hello!</button>;
+    
+}
+
+export default Button;
